f4effb81c04b82629ead518ec425a1c8f0ff3552
Boris <risentveber@gmail.com>
2016-11-30 02:46:29 +0300

    create proto version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var join = require('path').join;
 var fullScriptsPath = join(process.cwd(), 'scripts.json');
 var errorExit = require('./lib/errorExit');
 var run = require('./lib/run');
+var commandName = process.argv[2];
 
 try {
     var scripts = require(fullScriptsPath);
@@ -11,9 +12,7 @@ try {
     errorExit(e.toString());
 }
 
-!process.argv[2] && errorExit('No script name presented!');
-
-process.stdout.write('Starting script: \x1b[37m' + process.argv[2] + '\x1b[0m\n');
+!commandName && errorExit('No script name presented!');
 
 function buildPattern(name) {
     var re = /<%\w+%>/, match, data = [];
@@ -27,28 +26,39 @@ function buildPattern(name) {
     }
 }
 
-for(var l = scripts.length, i=0; i < l; i++) {
+for(var i = 0; i < scripts.length; i++) {
     pattern = buildPattern(scripts[i].name);
-    if (pattern.regexp.exec(process.env[2])) {
-        console.log('good')
-    } else
-        console.log('bad')
+
+    var match, data = [];
+    if (match = pattern.regexp.exec(commandName)) {
+        for(var p=0; p < pattern.params.length; p++) {
+           data[pattern.params[p]] = match[p + 1];
+        }
+        scripts[i] = JSON.parse(TemplateEngine(JSON.stringify(scripts[i]), data));
+        process.stdout.write('Starting script: \x1b[37m' + commandName + '\x1b[0m\n');
+        run(scripts[i], function (error, stdout, stderr) {
+            process.stderr.write(stderr);
+            process.stdout.write(stdout);
+            if(error !== null) {
+                errorExit('runtime error: ' + error);
+            }
+        });
+        process.stdout.write('Finishing script: \x1b[37m' + commandName + '\x1b[0m\n');
+        break;
+    } else {
+        //console.log('bad', commandName)
+    }
+
+    if (i + 1 === scripts.length) {
+        errorExit('No pattern for such script: \x1b[37m' + commandName + '\x1b[0m');
+    }
 }
 
-var TemplateEngine = function(tpl, data) {
-    var re = /<%([^%>]+)?%>/g, match;
+function TemplateEngine(tpl, data) {
+    var re = /<%([^%>]+)?%>/, match;
     while(match = re.exec(tpl)) {
         tpl = tpl.replace(match[0], data[match[1]])
     }
     return tpl;
 }
 
-run(scripts[0], function (error, stdout, stderr) {
-    process.stderr.write(stderr);
-    process.stdout.write(stdout);
-    if(error !== null) {
-        errorExit('runtime error: ' + error);
-    }
-});
-
-process.stdout.write('Finishing script: \x1b[37m' + process.argv[2] + '\x1b[0m\n');
