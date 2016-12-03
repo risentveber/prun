8d66f028f76306917e5bc27cf5a2825d624c6707
Boris <risentveber@gmail.com>
2016-12-04 01:32:54 +0300

    refactoring for pattern logic

--- index.js
+++ index.js
@@ -2,63 +2,41 @@
 
 var join = require('path').join;
 var fullScriptsPath = join(process.cwd(), 'scripts.json');
-var errorExit = require('./lib/errorExit');
 var run = require('./lib/run');
+var Pattern = require('./lib/Pattern');
+var err = require('./lib/logger').err;
+var exit = require('./lib/logger').exit;
+var log = require('./lib/logger').log;
+var m = require('./lib/logger').modifiers;
+
 var commandName = process.argv[2];
 
 try {
     var scripts = require(fullScriptsPath);
 } catch(e) {
-    errorExit(e.toString());
+    exit(e.toString());
 }
 
-!commandName && errorExit('No script name presented!');
-
-function buildPattern(name) {
-    var re = /<%\w+%>/, match, data = [];
-    while(match = re.exec(name)) {
-        name = name.replace(match[0], '(\\w+)');
-        data.push(match[0].slice(2, -2))
-    }
-    return {
-        regexp: new RegExp('^' + name + '$'),
-        params: data
-    }
-}
+!commandName && exit('No script name presented!');
 
 for(var i = 0; i < scripts.length; i++) {
-    pattern = buildPattern(scripts[i].name);
+    var pattern = new Pattern(scripts[i]);
 
-    var match, data = [];
-    if (match = pattern.regexp.exec(commandName)) {
-        for(var p=0; p < pattern.params.length; p++) {
-           data[pattern.params[p]] = match[p + 1];
-        }
-        scripts[i] = JSON.parse(TemplateEngine(JSON.stringify(scripts[i]), data));
-        process.stdout.write('Starting script: \x1b[37m' + commandName + '\x1b[0m\n');
-        run(scripts[i], function (error, stdout, stderr) {
+    if (pattern.matches(commandName)) {
+        log('Starting script: ');
+        log(commandName + '\n', m.FgRed);
+        run(pattern.getDataForExecution(), function (error, stdout, stderr) {
             process.stderr.write(stderr);
             process.stdout.write(stdout);
-            if(error !== null) {
-                errorExit('runtime error: ' + error);
-            }
+            error !== null && exit('runtime error: ' + error);
         });
-        process.stdout.write('Finishing script: \x1b[37m' + commandName + '\x1b[0m\n');
+        log('Finishing script: ');
+        log(commandName + '\n', m.FgRed);
         break;
-    } else {
-        //console.log('bad', commandName)
     }
 
     if (i + 1 === scripts.length) {
-        errorExit('No pattern for such script: \x1b[37m' + commandName + '\x1b[0m');
+        err('No pattern for such script: ');
+        exit(commandName + '\n', m.FgRed);
     }
 }
-
-function TemplateEngine(tpl, data) {
-    var re = /<%([^%>]+)?%>/, match;
-    while(match = re.exec(tpl)) {
-        tpl = tpl.replace(match[0], data[match[1]])
-    }
-    return tpl;
-}
-
